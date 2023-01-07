93e2fb5fb7f28674908dcd058ac2598409b31030
nhanaa <nhanpro7d@gmail.com>
2023-01-07 03:04:45 +0000

    Changed config

--- dist/routes/characters.js
+++ dist/routes/characters.js
@@ -8,23 +8,48 @@ const characters_json_1 = __importDefault(require("../../data/characters/charact
 const charactersRouter = (0, express_1.Router)();
 // GET all characters
 charactersRouter.get("/", (req, res) => {
-    res.status(200).json(characters_json_1.default);
+    try {
+        res.status(200).json(characters_json_1.default);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 // GET character by id
 charactersRouter.get("/id/:id", (req, res) => {
     const { id } = req.params;
-    res.status(200).json(characters_json_1.default.filter((character) => character.id === id));
+    try {
+        res.status(200).json(characters_json_1.default.filter((character) => character.id === id));
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 // GET masters characters
 charactersRouter.get("/masters", (req, res) => {
-    res.status(200).json(characters_json_1.default.filter((character) => character.role === "master"));
+    try {
+        res.status(200).json(characters_json_1.default.filter((character) => character.role === "master"));
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 // GET servants characters
 charactersRouter.get("/servants", (req, res) => {
-    res.status(200).json(characters_json_1.default.filter((character) => character.role === "servant"));
+    try {
+        res.status(200).json(characters_json_1.default.filter((character) => character.role === "servant"));
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 // GET non-participant characters
 charactersRouter.get("/non-participant", (req, res) => {
-    res.status(200).json(characters_json_1.default.filter((character) => character.role === "non-participant"));
+    try {
+        res.status(200).json(characters_json_1.default.filter((character) => character.role === "non-participant"));
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 exports.default = charactersRouter;
