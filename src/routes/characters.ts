2dce69e1df897bc29959f6008fa0d4b361e2afcf
nhanaa <nhanpro7d@gmail.com>
2023-01-07 02:55:24 +0000

    Renamed character interface to match the regular expression

--- src/routes/characters.ts
+++ src/routes/characters.ts
@@ -3,7 +3,7 @@ import charactersJSON from '../../data/characters/characters.json';
 
 const charactersRouter: Router = Router();
 
-interface character {
+interface Character {
   id?: string;
   role?: string;
   image?: string;
@@ -11,30 +11,53 @@ interface character {
 
 // GET all characters
 charactersRouter.get("/", (req, res) => {
-  res.status(200).json(charactersJSON);
+  try {
+    res.status(200).json(charactersJSON);
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 // GET character by id
 charactersRouter.get("/id/:id", (req, res) => {
   const {id} = req.params;
-
-  res.status(200).json(charactersJSON.filter((character: character) => character.id === id));
+  try {
+    res.status(200).json(charactersJSON.filter((character: Character) => character.id === id));
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 // GET masters characters
 charactersRouter.get("/masters", (req, res) => {
-  res.status(200).json(charactersJSON.filter((character: character) => character.role === "master"))
+  try {
+    res.status(200).json(charactersJSON.filter((character: Character) => character.role === "master"))
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 // GET servants characters
 charactersRouter.get("/servants", (req, res) => {
-  res.status(200).json(charactersJSON.filter((character: character) => character.role === "servant"))
+  try {
+    res.status(200).json(charactersJSON.filter((character: Character) => character.role === "servant"))
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 });
 
 // GET non-participant characters
 charactersRouter.get("/non-participant", (req, res) => {
-  res.status(200).json(charactersJSON.filter((character: character) => character.role === "non-participant"))
+  try {
+    res.status(200).json(charactersJSON.filter((character: Character) => character.role === "non-participant"))
+  }
+  catch (err) {
+    res.status(500).send(err)
+  }
 })
-
 
 export default charactersRouter;
